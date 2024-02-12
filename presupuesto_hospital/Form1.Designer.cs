40278385149c4c536a1558798869aa47ce79c96a
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 08:50:26 -0400

    Agregar archivos de proyecto.

---
+++ presupuesto_hospital/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace presupuesto_hospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lebel1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Administración = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_presupuesto = new System.Windows.Forms.TextBox();
+            this.txt_Emergencia = new System.Windows.Forms.TextBox();
+            this.txt_Cirugía = new System.Windows.Forms.TextBox();
+            this.txt_Suministros = new System.Windows.Forms.TextBox();
+            this.txt_Administracion = new System.Windows.Forms.TextBox();
+            this.txt_Laboratorio = new System.Windows.Forms.TextBox();
+            this.txt_Imágenes_Diagnosticas = new System.Windows.Forms.TextBox();
+            this.btn_Procesar = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Presupuesto del hospital distribuido";
+            // 
+            // Lebel1
+            // 
+            this.Lebel1.AutoSize = true;
+            this.Lebel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lebel1.Location = new System.Drawing.Point(120, 84);
+            this.Lebel1.Name = "Lebel1";
+            this.Lebel1.Size = new System.Drawing.Size(133, 25);
+            this.Lebel1.TabIndex = 1;
+            this.Lebel1.Text = "Presupuesto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(155, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Emergencia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(155, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cirugía ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Suministros";
+            // 
+            // txt_Administración
+            // 
+            this.txt_Administración.AutoSize = true;
+            this.txt_Administración.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Administración.Location = new System.Drawing.Point(155, 376);
+            this.txt_Administración.Name = "txt_Administración";
+            this.txt_Administración.Size = new System.Drawing.Size(154, 25);
+            this.txt_Administración.TabIndex = 5;
+            this.txt_Administración.Text = "Administración";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(155, 449);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Laboratorio";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(109, 523);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(236, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Imágenes Diagnosticas";
+            // 
+            // txt_presupuesto
+            // 
+            this.txt_presupuesto.Location = new System.Drawing.Point(274, 84);
+            this.txt_presupuesto.Name = "txt_presupuesto";
+            this.txt_presupuesto.Size = new System.Drawing.Size(100, 22);
+            this.txt_presupuesto.TabIndex = 8;
+            // 
+            // txt_Emergencia
+            // 
+            this.txt_Emergencia.Location = new System.Drawing.Point(409, 149);
+            this.txt_Emergencia.Name = "txt_Emergencia";
+            this.txt_Emergencia.Size = new System.Drawing.Size(117, 22);
+            this.txt_Emergencia.TabIndex = 9;
+            // 
+            // txt_Cirugía
+            // 
+            this.txt_Cirugía.Location = new System.Drawing.Point(409, 237);
+            this.txt_Cirugía.Name = "txt_Cirugía";
+            this.txt_Cirugía.Size = new System.Drawing.Size(117, 22);
+            this.txt_Cirugía.TabIndex = 10;
+            // 
+            // txt_Suministros
+            // 
+            this.txt_Suministros.Location = new System.Drawing.Point(409, 309);
+            this.txt_Suministros.Name = "txt_Suministros";
+            this.txt_Suministros.Size = new System.Drawing.Size(117, 22);
+            this.txt_Suministros.TabIndex = 11;
+            // 
+            // txt_Administracion
+            // 
+            this.txt_Administracion.Location = new System.Drawing.Point(409, 375);
+            this.txt_Administracion.Name = "txt_Administracion";
+            this.txt_Administracion.Size = new System.Drawing.Size(117, 22);
+            this.txt_Administracion.TabIndex = 12;
+            // 
+            // txt_Laboratorio
+            // 
+            this.txt_Laboratorio.Location = new System.Drawing.Point(409, 448);
+            this.txt_Laboratorio.Name = "txt_Laboratorio";
+            this.txt_Laboratorio.Size = new System.Drawing.Size(117, 22);
+            this.txt_Laboratorio.TabIndex = 13;
+            this.txt_Laboratorio.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txt_Imágenes_Diagnosticas
+            // 
+            this.txt_Imágenes_Diagnosticas.Location = new System.Drawing.Point(409, 523);
+            this.txt_Imágenes_Diagnosticas.Name = "txt_Imágenes_Diagnosticas";
+            this.txt_Imágenes_Diagnosticas.Size = new System.Drawing.Size(117, 22);
+            this.txt_Imágenes_Diagnosticas.TabIndex = 14;
+            // 
+            // btn_Procesar
+            // 
+            this.btn_Procesar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn_Procesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Procesar.ForeColor = System.Drawing.Color.Black;
+            this.btn_Procesar.Location = new System.Drawing.Point(618, 199);
+            this.btn_Procesar.Name = "btn_Procesar";
+            this.btn_Procesar.Size = new System.Drawing.Size(165, 60);
+            this.btn_Procesar.TabIndex = 15;
+            this.btn_Procesar.Text = "Procesar distribución";
+            this.btn_Procesar.UseVisualStyleBackColor = false;
+            this.btn_Procesar.Click += new System.EventHandler(this.btn_Procesar_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.ForeColor = System.Drawing.Color.Black;
+            this.btn_Limpiar.Location = new System.Drawing.Point(618, 303);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(165, 50);
+            this.btn_Limpiar.TabIndex = 16;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.ForeColor = System.Drawing.Color.Black;
+            this.btn_Salir.Location = new System.Drawing.Point(618, 403);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(165, 44);
+            this.btn_Salir.TabIndex = 17;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = false;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(840, 568);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Procesar);
+            this.Controls.Add(this.txt_Imágenes_Diagnosticas);
+            this.Controls.Add(this.txt_Laboratorio);
+            this.Controls.Add(this.txt_Administracion);
+            this.Controls.Add(this.txt_Suministros);
+            this.Controls.Add(this.txt_Cirugía);
+            this.Controls.Add(this.txt_Emergencia);
+            this.Controls.Add(this.txt_presupuesto);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_Administración);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Lebel1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lebel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txt_Administración;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_presupuesto;
+        private System.Windows.Forms.TextBox txt_Emergencia;
+        private System.Windows.Forms.TextBox txt_Cirugía;
+        private System.Windows.Forms.TextBox txt_Suministros;
+        private System.Windows.Forms.TextBox txt_Administracion;
+        private System.Windows.Forms.TextBox txt_Laboratorio;
+        private System.Windows.Forms.TextBox txt_Imágenes_Diagnosticas;
+        private System.Windows.Forms.Button btn_Procesar;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+    }
+}
+
---
+++ presupuesto_hospital/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace presupuesto_hospital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Procesar_Click(object sender, EventArgs e)
+        {
+            double presupuesto, Emergencia, Cirugía, Suministros, Administración, Laboratorio, Imágenes;
+
+            presupuesto=double.Parse(txt_presupuesto.Text);
+
+            Emergencia = (presupuesto * 30) / 100;
+            Cirugía = (presupuesto * 15)/ 100;
+            Suministros= (presupuesto * 25)/100;
+            Administración = (presupuesto * 18) / 100;
+            Laboratorio = (presupuesto * 5) / 100;
+            Imágenes = (presupuesto * 7) / 100; 
+
+            txt_Emergencia.Text = Emergencia.ToString();
+            txt_Cirugía.Text = Cirugía.ToString();
+            txt_Suministros.Text = Suministros.ToString();
+            txt_Administracion.Text = Administración.ToString();
+            txt_Laboratorio.Text = Laboratorio.ToString();
+            txt_Imágenes_Diagnosticas.Text = Imágenes.ToString();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_presupuesto.Clear();
+            txt_Emergencia.Clear();
+            txt_Cirugía.Clear();
+            txt_Suministros.Clear();
+            txt_Administracion.Clear();
+            txt_Laboratorio.Clear();
+            txt_Imágenes_Diagnosticas.Clear();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
